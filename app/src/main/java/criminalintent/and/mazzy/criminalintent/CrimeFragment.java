f1790c72ed8109fe3e4c1aadad7b794683bcfa58
maxdk9 <maxdk9@gmail.com>
2018-10-26 18:00:34 +0300

    crimefragment

---
+++ app/src/main/java/criminalintent/and/mazzy/criminalintent/CrimeFragment.java
@@ -0,0 +1,17 @@
+package criminalintent.and.mazzy.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+    Crime mCrime;
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+}
